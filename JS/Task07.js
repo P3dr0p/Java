9d4f3ea44e969187751289f612f80ef84d5b2ff6
Pedro Paulo Ramalho de Almeida <108875939+P3dr0p@users.noreply.github.com>
2023-03-03 08:04:48 -0300

    Add files via upload

--- JS/Task07.js
+++ JS/Task07.js
@@ -6,3 +6,10 @@ Atividade para fazer:
 Para um dado número n, o programa testa todos os números de 1 ate n.
 
 */
+
+const number = parseInt(prompt('Digite um número'))
+
+for(let i = 0; i <= number; i++) {
+    number % i === 0 ? document.getElementById('info').innerHTML += `${i}`
+    : '';
+}
